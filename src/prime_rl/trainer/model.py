a19ff671f557fdf6213a6cbe7874573fac4da13d
celeste <parafactual@gmail.com>
2025-08-30 13:28:41 -0700

    Simplify get_model with model_cls var

--- src/prime_rl/trainer/model.py
+++ src/prime_rl/trainer/model.py
@@ -51,18 +51,12 @@ def get_model(config: ModelConfig) -> nn.Module:
     )
     config_model.use_cache = False
     
-    if config.liger_kernel:
-        model = AutoLigerKernelForCausalLM.from_pretrained(
-            pretrained_model_name_or_path=config.name,
-            config=config_model,
-            trust_remote_code=config.trust_remote_code,
-        )
-    else:
-        model = AutoModelForCausalLM.from_pretrained(
-            pretrained_model_name_or_path=config.name,
-            config=config_model,
-            trust_remote_code=config.trust_remote_code,
-        )
+    model_cls = AutoLigerKernelForCausalLM if config.liger_kernel else AutoModelForCausalLM
+    model = model_cls.from_pretrained(
+        pretrained_model_name_or_path=config.name,
+        config=config_model,
+        trust_remote_code=config.trust_remote_code,
+    )
 
     return model
 
